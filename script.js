f03f4f723e07580f860b9ce32661493abd4bff3f
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-03-25 19:56:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this.species = species;
+  }
+  get species() {
+    return this._species;
+  }
+  set species(value) {
+    this._species = value;
+  }
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal { 
+	 constructor() {
+    super('Dog');
+  }
+  bark() {
+    console.log('woof');
+  }
 
-class Cat extends Animal {}
+	
+}
+
+class Cat extends Animal {
+	super('Cat'); 
+  }
+  purr() {
+    console.log('purr');
+  }
+
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
